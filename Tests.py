6a70ebdc86fd99c5c2cf27175565689936e06414
sos-mislom <60609474+sos-mislom@users.noreply.github.com>
2024-06-16 20:12:53 +0500

    Update Tests.py

--- Tests.py
+++ Tests.py
@@ -1,32 +1,29 @@
 import librosa
-import matplotlib.pyplot as plt
 import numpy as np
+import json
+import random
+def analyze_song(file_path):
+    y, sr = librosa.load(file_path)
+    tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+    beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+    
+    extended = []
+    
+    for beat in  beat_times.tolist():
+        extended.append(beat)
+        num_additional_beats = random.randint(1, 2)
+        for _ in range(num_additional_beats):
+            extended.append(beat + 0.1)
+        
+    analysis_data = {
+        'tempo': float(tempo),
+        'beats': extended
+    }
+    
+    with open('song_analysis.json', 'w') as json_file:
+        json.dump(analysis_data, json_file)
 
-from Helpers import PATH
+    print(f'Анализ завершен. Темп: {tempo:.2f} BPM')
+    print(f'Результаты сохранены в song_analysis.json')
 
-# Загрузка аудиофайла
-y, sr = librosa.load(PATH)
-onset_env = librosa.onset.onset_strength(y=y, sr=sr)
-pulse = librosa.beat.plp(onset_envelope=onset_env, sr=sr)
-tempo, beats = librosa.beat.beat_track(onset_envelope=onset_env)
-beats_plp = np.flatnonzero(librosa.util.localmax(pulse))
-
-import matplotlib.pyplot as plt
-fig, ax = plt.subplots(nrows=2, sharex=True, sharey=True)
-times = librosa.times_like(onset_env, sr=sr)
-
-ax[0].plot(times, librosa.util.normalize(onset_env), label='Onset strength')
-ax[0].vlines(times[beats], 0, 1, alpha=0.5, color='r', linestyle='--', label='Beats')
-ax[0].legend()
-ax[0].set(title='librosa.beat.beat_track')
-ax[0].label_outer()
-
-times = librosa.times_like(pulse, sr=sr)
-ax[1].plot(times, librosa.util.normalize(pulse),label='PLP')
-ax[1].vlines(times[beats_plp], 0, 1, alpha=0.5, color='r', linestyle='--', label='PLP Beats')
-ax[1].legend()
-ax[1].set(title='librosa.beat.plp', xlim=[5, 20])
-ax[1].xaxis.set_major_formatter(librosa.display.TimeFormatter())
-
-
-plt.show()
+analyze_song(r'C:\Users\User\Desktop\audio.mp3')
